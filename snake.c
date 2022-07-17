773f2873c2c3aa753677bc91e1908bca5a0c694b
antonyPJ <seantonyssiq@gmail.com>
2022-07-17 14:58:20 -0300

    VS2

--- snake.c
+++ snake.c
@@ -4,43 +4,58 @@
 #include <windows.h>
 
 // Variáveis que serão usadas no programa
-int i, k, j, x, y, altura = 20, largura = 20, comidaX, comidaY, tecla, fimloop;
+int x, y, altura = 20, largura = 20, comidaX, comidaY, pontos, recorde, fimloop;
 int caudaX[100], caudaY[100];
-int chkcauda, print;
+int chkcauda;
 
-// Função responsável por criar as bordas do tabuleiro, a cobra "@" e sua cauda "o" e a comida "*"
+FILE *arq; 
+
+// Struct usado para armazenar o nome da pessoa que tem a maior pontuação
+struct ranking {
+    char nome[20];
+};
+struct ranking pessoa;
+
+// Função responsável por criar as bordas do tabuleiro, a cobra "@", sua cauda "o", a comida "*"
 void tabuleiro() {
+    arq=fopen("recorde.txt", "r");
+    fscanf(arq, "%d%s", &recorde, &pessoa.nome);
+    fclose(arq);
+
     system("cls");
+    // Printa as bordas laterais
     for(int i = 0; i < altura; i++) {
-        printf("$");
+        printf("#");
         for(int k = 0; k < largura; k++) {
+            // Printa as bordas superiores e inferiores
             if(i == 0 || i == altura-1) {
-                printf("$");
+                printf("#");
             }else{
                 if (i == x && k == y) { 
                     printf("@");
                 } else if (i == comidaX && k == comidaY) { 
                     printf("*");
                 } else { 
-                    print = 0;
+                    int print = 0;
                     for (int j = 0; j < chkcauda; j++) {
                         if (caudaX[j] == k && caudaY[j] == i) {
                             printf("o");
                             print = 1;
                         }
-
                     }
                     if (!print) printf(" ");
                 }
             }
         }
-        printf("$\n");
-  }
+        printf("#\n");
+    }
+    printf("Pontos: %d   Recorde: %d  -  %s\n\n", pontos, recorde, pessoa.nome);
 }
 
 // Função responsável por estabelecer as coordenadas da cobra, e da comida
 void comida() {
     fimloop = 0;
+    pontos = 0;
 
     // Coordenadas da cobra
     x = largura / 2;
@@ -48,34 +63,17 @@ void comida() {
 
     // Coordenadas da comida randomizadas
     comidaX = rand() % largura;
-    comidaY = rand() % altura;  
+    comidaY = rand() % altura; 
    
 }
 
 // Função responsável por receber o input do usuário
-void controle() {
-    if (kbhit()) {
-        switch (getch()) {
-        case 'w':
-            tecla = 1;
-            break;
-        case 'a':
-            tecla = 2;
-            break;
-        case 's':
-            tecla = 3;
-            break;
-        case 'd':
-            tecla = 4;
-            break;
-        default:
-            break;
-        }
-    }
+void controle(int * tecla) {
 }
 
 // Função responsável por fazer o loop do jogo
-void gameloop() {  
+void gameloop(int * tecla) {  
+
     int posX = caudaX[0];
     int posY = caudaY[0];
     int pos2x, pos2y;
@@ -92,10 +90,31 @@ void gameloop() {
         posY = pos2y;
     }
 
-    Sleep(0.01);
 
     // Switch recebe a variavel tecla, e adiciona ou subtrai as coordenadas da cobra de acordo com a tecla
-    switch (tecla) {
+    if (kbhit()) {
+        switch (getch()) {
+        case 'w':
+            *tecla = 1;
+            break;
+        case 'a':
+            *tecla = 2;
+            break;
+        case 's':
+            *tecla = 3;
+            break;
+        case 'd':
+            *tecla = 4;
+            break;
+        case 'y':
+            fimloop = 1;
+            break;
+        default:
+            break;
+        }
+    }
+
+    switch (*tecla) {
     case 1:
         x--;
         break;
@@ -122,17 +141,45 @@ void gameloop() {
     // Se a cobra chegar nas mesmas coordenadas que a comida gerar nova comida
     if (x == comidaX && y == comidaY) {
         comidaX = rand() % largura;
-        comidaY = rand() % altura;     
+        comidaY = rand() % altura;
+        pontos++; 
         chkcauda++;
     }
 }
 
-void main() {
-    comida();
-    // Enquanto a variável fimloop estabelecida na função comida não for 0 o loop do jogo contínua
-    while (!fimloop) {
-        tabuleiro();
-        controle();
-        gameloop();
-    }
+int main() {
+    int ptrTecla;
+    inicio:
+        comida();
+        // Enquanto a variável fimloop estabelecida na função comida for 0 o loop do jogo contínua
+        while (fimloop == 0) {
+            tabuleiro();
+            gameloop(&ptrTecla);
+            // Se os pontos forem maior que o recorde ele vai se atualizando em tempo real sobreescrevendo o arquivo recorde.txt
+            if (pontos>recorde) {
+                arq=fopen("recorde.txt", "w");
+                fprintf(arq,"%d\n",pontos);
+                fclose(arq);
+            }
+        }
+        // Se os pontos forem iguais ao recorde será pedido o nome do jogador após o fim do loop, e será estabelecido um novo recorde
+        // printando então os pontos e nome do jogador no arquivo recorde.txt
+        if (pontos == recorde) {
+            printf("Digite seu nome: \n");
+            scanf("%s", &pessoa.nome);
+            arq=fopen("recorde.txt", "w");
+            fprintf(arq,"%d %s", pontos, pessoa.nome);
+            fclose(arq);
+        }
+        // Pede input do usuário se deseja jogar denovo voltando então ao ínicio do loop
+        char replay;
+        printf("Jogar novamente? Pressione Y: \n");
+        while (fimloop == 1) {
+            scanf("%c", &replay);
+            if(replay=='y'||replay=='Y') {
+                chkcauda = 0;
+                Sleep(500);
+                goto inicio;
+            }
+        }
 }
